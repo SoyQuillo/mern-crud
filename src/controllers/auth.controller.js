a97e8615623141e58ee5c814b7d90a99a32d4ef5
SoyQuillo <santiagojose190305@gmail.com>
2025-12-23 13:00:19 -0500

    creacion del token

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,18 +1,33 @@
 import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import { createAccessToken } from "../libs/jwt.js";
 
 export const register = async (req, res) => {
   const { email, password, username } = req.body;
 
   try {
+    const passwordHash = await bcrypt.hash(password, 10);
+
     const newUser = new User({
       username,
       email,
-      password,
+      password: passwordHash,
     });
+
     const userSaved = await newUser.save();
-    res.json(userSaved);
+    const token = await createAccessToken({ id: userSaved._id });
+
+    res.cookie("token", token);
+
+    res.json({
+      id: userSaved._id,
+      username: userSaved.username,
+      email: userSaved.email,
+      createdAt: userSaved.createdAt,
+      updatedAt: userSaved.updatedAt,
+    });
   } catch (e) {
-    console.log(e);
+    res.status(500).json({message: e.message })
   }
 };
 
